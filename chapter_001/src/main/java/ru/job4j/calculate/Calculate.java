e8ce9d80388e65f24e67da6199b7e24781fe6789
Roman Merkin <roman.merkin@gmail.com>
2018-06-25 22:04:26 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,18 @@
 package ru.job4j.calculate;
 
-public class Calculate 
-{
-	public static void main(String[] args)
-	{
+/**
+* writes "Hello world"
+* @author Roman Merkin (roman.merkin@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class Calculate {
+	/**
+	* First programm 
+	* @param args Array of string arguments
+	* @throws exteptions Exteptions are not granted
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
