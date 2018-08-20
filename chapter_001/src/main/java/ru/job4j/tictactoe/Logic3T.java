06086f8dc3c74d603cbae9cb14f9c9adf2cb2865
Roman Merkin <roman.merkin@gmail.com>
2018-08-20 15:19:50 +0300

    Крестики-нолики на JavaFX

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,5 +1,11 @@
 package ru.job4j.tictactoe;
 
+/**
+ * Class "Logic3T" contains logic for "TicTacToe" class
+ * @author Roman Merkin
+ * @version $Id$
+ * @since 0.1
+ */
 public class Logic3T {
     private final Figure3T[][] table;
 
@@ -7,16 +13,146 @@ public class Logic3T {
         this.table = table;
     }
 
+    /**
+     * Method "isWinnerX" detects if X is winner.
+     * @return true if X is winner
+     */
     public boolean isWinnerX() {
+        boolean result = false;
+        boolean isEqual = true;
 
-        return false;
+        for (int index = 0; index < table.length; index++) {
+            if (!table[index][index].hasMarkX()) {
+                isEqual = false;
+                break;
+            }
+        }
+
+        if (isEqual) {
+            result = true;
+        } else {
+            isEqual = true;
+        }
+
+        for (int index = 0; index < table.length; index++) {
+            if (!table[index][table.length - 1 - index].hasMarkX()) {
+                isEqual = false;
+                break;
+            }
+        }
+
+        if (isEqual) {
+            result = true;
+        } else {
+            isEqual = true;
+        }
+
+        for (int column = 0; column < table.length; column++) {
+            for (int row = 0; row < table.length; row++) {
+                if (!table[row][column].hasMarkX()) {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual) {
+                result = true;
+            } else {
+                isEqual = true;
+            }
+        }
+
+        for (int row = 0; row < table.length; row++) {
+            for (int column = 0; column < table.length; column++) {
+                if (!table[row][column].hasMarkX()) {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual) {
+                result = true;
+            } else {
+                isEqual = true;
+            }
+        }
+
+        return result;
     }
 
+
     public boolean isWinnerO() {
-        return false;
+        boolean result = false;
+        boolean isEqual = true;
+
+        for (int index = 0; index < table.length; index++) {
+            if (!table[index][index].hasMarkO()) {
+                isEqual = false;
+                break;
+            }
+        }
+
+        if (isEqual) {
+            result = true;
+        } else {
+            isEqual = true;
+        }
+
+        for (int index = 0; index < table.length; index++) {
+            if (!table[index][table.length - 1 - index].hasMarkO()) {
+                isEqual = false;
+                break;
+            }
+        }
+
+        if (isEqual) {
+            result = true;
+        } else {
+            isEqual = true;
+        }
+
+        for (int column = 0; column < table.length; column++) {
+            for (int row = 0; row < table.length; row++) {
+                if (!table[row][column].hasMarkO()) {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual) {
+                result = true;
+            } else {
+                isEqual = true;
+            }
+        }
+
+        for (int row = 0; row < table.length; row++) {
+            for (int column = 0; column < table.length; column++) {
+                if (!table[row][column].hasMarkO()) {
+                    isEqual = false;
+                    break;
+                }
+            }
+            if (isEqual) {
+                result = true;
+            } else {
+                isEqual = true;
+            }
+        }
+
+        return result;
     }
 
     public boolean hasGap() {
-        return true;
+        boolean result = false;
+
+        LOOP_ROW:
+        for (int row = 0; row < table.length; row++) {
+            for (int column = 0; column < table.length; column++) {
+                if (!(table[row][column].hasMarkX() || table[row][column].hasMarkO())) {
+                    result = true;
+                    break LOOP_ROW;
+                }
+            }
+        }
+
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
+++ chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -29,7 +29,7 @@ public class Logic3TTest {
     }
 
     @Test
-    public void whenHasGas() {
+    public void whenHasGap() {
         Figure3T[][] table = {
                 {new Figure3T(true), new Figure3T(), new Figure3T()},
                 {new Figure3T(), new Figure3T(true), new Figure3T()},
@@ -37,5 +37,16 @@ public class Logic3TTest {
         };
         Logic3T login = new Logic3T(table);
         assertThat(login.hasGap(), is(true));
+    }
+
+    @Test
+    public void whenHasNoGap() {
+        Figure3T[][] table = {
+                {new Figure3T(true), new Figure3T(false), new Figure3T(false)},
+                {new Figure3T(false), new Figure3T(true), new Figure3T(false)},
+                {new Figure3T(false), new Figure3T(false), new Figure3T(true)},
+        };
+        Logic3T login = new Logic3T(table);
+        assertThat(login.hasGap(), is(false));
     }
 }
