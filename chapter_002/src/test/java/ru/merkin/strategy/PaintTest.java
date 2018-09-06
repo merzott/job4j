0f6ca2634851cd78d505b8b74e02c81665b827eb
Roman Merkin <roman.merkin@gmail.com>
2018-09-06 16:30:56 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/merkin/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/merkin/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.merkin.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -16,6 +18,21 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     /**
      * Test for "draw" method
      *
@@ -30,11 +47,25 @@ public class PaintTest {
                 .append("####").append(System.lineSeparator())
                 .append(System.lineSeparator()
                 );
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square(4));
         assertThat(new String(out.toByteArray()), is(expect.toString()));
-        System.setOut(stdout);
+    }
+
+    /**
+     * Test for "draw" method
+     *
+     * Checks that paint.draw(Triangle(4)) draws correct.
+     */
+    @Test
+    public void drawTriangleSizeFourThanOk() {
+        StringBuilder expect = new StringBuilder()
+                .append("   #   ").append(System.lineSeparator())
+                .append("  # #  ").append(System.lineSeparator())
+                .append(" #   # ").append(System.lineSeparator())
+                .append("#######").append(System.lineSeparator())
+                .append(System.lineSeparator()
+                );
+        new Paint().draw(new Triangle(4));
+        assertThat(new String(out.toByteArray()), is(expect.toString()));
     }
 }
